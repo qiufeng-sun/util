4dad3b8b90740cc0b1fbe8eda256e49406cfeef1
sunxiguang <sunxiguang@xiaomi.com>
2017-03-17 18:35:20 +0800

    update etcd

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -3,34 +3,36 @@ package etcd
 import (
 	"time"
 
-	"github.com/astaxie/beego/config"
-
 	"util/logs"
 )
 
 var _ = logs.Debug
 
 //
-func RegAndWatchs(logMsg string, confd config.Configer, fUpdate func(svc string, svcAddrs []string)) {
+type SrvCfg struct {
+	EtcdAddrs []string // etcd地址数组
+
+	SrvAddr      string // 服务监听addr
+	SrvRegPath   string // etcd上服务位置
+	SrvRegUpTick int64  // etcd上服务更新ttl间隔(ms)
+
+	WatchPaths []string // 需监听的服务器位置
+}
+
+//
+func RegAndWatchs(logMsg string, cfg *SrvCfg, fUpdate func(svc string, svcAddrs []string)) {
 	logs.Info("etcd init start: %v", logMsg)
 	defer logs.Info("etcd init end: %v", logMsg)
 
-	//
-	etcdAddrs := confd.Strings("etcd_addrs")
-
 	// register
-	etcdRegAddr := confd.String("server_addr")
-	etcdRegPath := confd.String("etcd_reg_path")
-	etcdRegUpTick := time.Duration(confd.DefaultInt64("etcd_reg_uptick", 1000)) * time.Millisecond
+	etcdRegUpTick := time.Duration(cfg.SrvRegUpTick) * time.Millisecond
 	etcdRegTTL := etcdRegUpTick * 3
 
-	r := NewEtcdRegister(etcdAddrs, etcdRegUpTick, etcdRegTTL)
-	r.Register(etcdRegPath, etcdRegAddr, "")
+	r := NewEtcdRegister(cfg.EtcdAddrs, etcdRegUpTick, etcdRegTTL)
+	r.Register(cfg.SrvRegPath, cfg.SrvAddr, "")
 
 	// services watched
-	etcdWatchPaths := confd.Strings("etcd_watch_path")
-
-	if len(etcdWatchPaths) > 0 {
-		AddWatchs(etcdAddrs, etcdWatchPaths, fUpdate)
+	if len(cfg.WatchPaths) > 0 {
+		AddWatchs(cfg.EtcdAddrs, cfg.WatchPaths, fUpdate)
 	}
 }
